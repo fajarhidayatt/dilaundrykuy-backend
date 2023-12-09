16c3138bcfeb68ed8db3834578df20fe7d3f4bd0
fajarhidayatt <fadjhar10@gmail.com>
2023-12-09 21:01:21 +0700

    create api for register, login, getAllUsers

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/user', [UserController::class, 'readAll']);
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
